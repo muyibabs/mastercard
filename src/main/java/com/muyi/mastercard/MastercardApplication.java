bac762d7944b9bd6eb0aa2d5815343aaddb69320
MuyiBabs <obabayomi@socketworksng.com>
2020-10-07 19:29:37 -0400

    Initial commit

---
+++ src/main/java/com/muyi/mastercard/MastercardApplication.java
@@ -0,0 +1,13 @@
+package com.muyi.mastercard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastercardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastercardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muyi/mastercard/MastercardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muyi.mastercard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastercardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
